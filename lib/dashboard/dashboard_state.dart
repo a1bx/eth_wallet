ddfcef15a38eed3286e079c604b05ec5a7d9554b
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-05-19 22:46:29 +0300

    update

--- lib/dashboard/dashboard_state.dart
+++ lib/dashboard/dashboard_state.dart
@@ -4,3 +4,9 @@ part of 'dashboard_bloc.dart';
 sealed class DashboardState {}
 
 final class DashboardInitial extends DashboardState {}
+
+class DashboardLoadingState extends DashboardState{}
+
+class DashboardErrorState extends DashboardState{}
+
+class DashboardSuccessState extends DashboardState{}
