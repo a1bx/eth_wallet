e19d8b85a5a981511074dba3ccb305d7b1098007
Jeremiah Nyok <jeremih@students.uonbi.ac.ke>
2024-05-20 19:02:57 +0300

    update

--- lib/features/dashboard/ui/dashboard.dart
+++ lib/features/dashboard/ui/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:eth_wallet/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -12,30 +13,93 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.accent,
       appBar: AppBar(
         title: Text("Eth Wallet"),
-        backgroundColor: ,
+        backgroundColor:AppColors.accent,
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 16,vertical: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal:12,vertical: 8),
+                decoration: BoxDecoration(
+                  color: Colors.white,borderRadius: BorderRadius.circular(12)
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset("assets/Ethereum logo.svg",
+                      height: 50,
+                      width: 50,
+                    ),
+                    // Image.asset("assets/Ethereum-logo.svg"),
+                    const SizedBox(width: 16), // Add spacing between image and text
+                    Text("100 ETH",style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold
+                    ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height:12),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SvgPicture.asset("assets/Ethereum logo.svg",
+                Expanded(child: Container(
                   height: 50,
-                  width: 50,
-                ),
-                // Image.asset("assets/Ethereum-logo.svg"),
-                const SizedBox(width: 16), // Add spacing between image and text
-                Text("100 ETH",style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
+                  color: Colors.teal
+                  ),
+                  child: Center(
+                    child: Text("+ Deposit",style: TextStyle(
+                      color: Colors.redAccent, fontSize:24,fontWeight: FontWeight.bold
+                    ),
+                    ),
+                  ),
+                )),
+                const SizedBox(width:8),
+                Expanded(child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
+                      color: Colors.redAccent
+                  ),
+                  child: Center(
+                    child: Text("- Withdraw",style: TextStyle(
+                        color: Colors.tealAccent, fontSize:24,fontWeight: FontWeight.bold
+                    ),
+                    ),
+                  ),
+                ))
               ],
-            )
+            ),
+            const SizedBox(
+              height: 20),
+            Text("Transactions",style: TextStyle (
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ) ,),
+            Expanded(child: ListView(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 12,horizontal:12 ),
+                  decoration:BoxDecoration (
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.white
+                  ),
+                  child: Row(
+                    children: [Text("1 ETH")
+                    ],
+                  ),
+                )
+              ],
+            ))
+
           ],
         ),
       ),
